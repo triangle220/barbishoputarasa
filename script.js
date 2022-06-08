2503890380f881a59617dcb94cdad22ff2413b5a
triangle220 <92473111+triangle220@users.noreply.github.com>
2022-06-08 18:22:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function openWindow(){
+    let top = window.pageYOffset;
+    top = top + 100;
+    document.getElementById('window1').style.display = 'block';
+    document.getElementById('window1').style.top = top +'px';
+  }
+  function closeWindow(){
+    let top = window.pageYOffset;
+    top = top + 100;
+    document.getElementById('window1').style.display = 'none';
+    document.getElementById('window1').style.top = top +'px';
+  }
+  function openWindow1(){
+    let top = window.pageYOffset;
+    top = top + 100;
+    document.getElementById('window2').style.display = 'block';
+    document.getElementById('window2').style.top = top +'px';
+  }
+  function closeWindow2(){
+    let top = window.pageYOffset;
+    top = top + 100;
+    document.getElementById('window2').style.display = 'none';
+    document.getElementById('window2').style.top = top +'px';
+  }
